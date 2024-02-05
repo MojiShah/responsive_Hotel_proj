640fa982a2317265488c7dc7cdf861a98340362d
Mojtaba Charchian <mojishah70@gmail.com>
2024-02-05 18:58:36 +0330

    responsive_hotel

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector('.btn');
+let list = document.querySelector('.list');
+
+
+
+btn.addEventListener('click',()=>{
+    list.classList.toggle('hide');
+})
+console.log(list);
